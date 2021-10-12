9048d149946d887155e34851b40ab9c4ba925e56
Nuecb56 <92362544+Nuecb56@users.noreply.github.com>
2021-10-12 19:21:14 +0530

    Create rad2.sh

---
+++ rad2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt-get update && apt-get -y install -y curl gnupg
+curl -L https://www.nimiq.com/nimiq-signing-key.pub | apt-key add -
+VERSION=1.5.7-1
+cd / && curl -O https://repo.nimiq.com/deb/pool/stable/main/n/nimiq/nimiq_${VERSION}_amd64.deb.asc
+cd / && curl -O https://repo.nimiq.com/deb/pool/stable/main/n/nimiq/nimiq_${VERSION}_amd64.deb.sha256sum
+curl -O https://repo.nimiq.com/deb/pool/stable/main/n/nimiq/nimiq_${VERSION}_amd64.deb 
+sha256sum -c nimiq_${VERSION}_amd64.deb.sha256sum 
+dpkg -i nimiq_${VERSION}_amd64.deb 
+rm -f nimiq_${VERSION}_amd64.deb nimiq_${VERSION}_amd64.deb.asc nimiq_${VERSION}_amd64.deb.sha256sum
+PROTO=dumb
+HOST=local.localhost
+PORT=80
+WALLADDR=NQ10GFRP80K4UQ19ASS0YLJCC43F6JV1P4RM
+POOL=pool.acemining.co:8443
+TYPE=light
+if [ "$PROTO" = "ws" ]
+then /usr/share/nimiq/app/node /usr/share/nimiq/app/index.js --protocol=ws --host={$HOST} --port=${PORT} --miner --pool=${POOL} --type=${TYPE} --wallet-address=${WALLADDR}
+else /usr/share/nimiq/app/node /usr/share/nimiq/app/index.js --protocol=dumb --miner --pool=${POOL} --type=${TYPE} --wallet-address=${WALLADDR}
+fi
+
+exec "$@"

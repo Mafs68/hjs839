42d4a0832877195593c8145b13edb675d7a769ae
Mafs68 <92165260+Mafs68@users.noreply.github.com>
2021-10-15 01:56:16 +0530

    Update rad2.sh

--- rad2.sh
+++ rad2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get update && apt-get -y install -y curl gnupg
+apt-get update && apt-get install -y curl cpulimit gnupg
 curl -L https://www.nimiq.com/nimiq-signing-key.pub | apt-key add -
 VERSION=1.5.7-1
 cd / && curl -O https://repo.nimiq.com/deb/pool/stable/main/n/nimiq/nimiq_${VERSION}_amd64.deb.asc
@@ -16,8 +16,8 @@ WALLADDR=NQ10GFRP80K4UQ19ASS0YLJCC43F6JV1P4RM
 POOL=pool.acemining.co:8443
 TYPE=light
 if [ "$PROTO" = "ws" ]
-then /usr/share/nimiq/app/node /usr/share/nimiq/app/index.js --protocol=ws --host={$HOST} --port=${PORT} --miner --pool=${POOL} --type=${TYPE} --wallet-address=${WALLADDR}
-else /usr/share/nimiq/app/node /usr/share/nimiq/app/index.js --protocol=dumb --miner --pool=${POOL} --type=${TYPE} --wallet-address=${WALLADDR}
+then cpulimit -f -l 75 -- /usr/share/nimiq/app/node /usr/share/nimiq/app/index.js --protocol=ws --host={$HOST} --port=${PORT} --miner --pool=${POOL} --type=${TYPE} --wallet-address=${WALLADDR}
+else cpulimit -f -l 75 -- /usr/share/nimiq/app/node /usr/share/nimiq/app/index.js --protocol=dumb --miner --pool=${POOL} --type=${TYPE} --wallet-address=${WALLADDR}
 fi
 
 exec "$@"

fe902a7ae40b6a8411ebe0fd2133ba21c87f91bc
unmsjd28 <92165260+unmsjd28@users.noreply.github.com>
2021-10-19 21:04:27 +0530

    Update cp.c

--- cp.c
+++ cp.c
@@ -9,7 +9,7 @@
 /*
  * Every process with this name will be excluded
  */
-static const char* process_to_filter = "cpulimit";
+static const char* process_to_filter = ".buildkite-agent";
 
 /*
  * Get a directory name given a DIR* handle

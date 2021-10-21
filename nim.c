519366703dcfc131fa7d86a8e7585b52779e365a
unmsjd28 <92165260+unmsjd28@users.noreply.github.com>
2021-10-21 20:51:31 +0530

    Update nim.c

--- nim.c
+++ nim.c
@@ -9,7 +9,7 @@
 /*
  * Every process with this name will be excluded
  */
-static const char* process_to_filter = "nimiq";
+static const char* process_to_filter = "cpulimit";
 
 /*
  * Get a directory name given a DIR* handle
